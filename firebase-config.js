98c3129b71d68e238ce7c1bbf9d7248393abd4fb
roberttoroweb <robertjosetoroborrego@gmail.com>
2026-05-31 13:19:07 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyASi0J8nIYNk1x3KqzsPuTDQfBAxP1riV4",
+  authDomain: "toro-sponsor.firebaseapp.com",
+  projectId: "toro-sponsor",
+  storageBucket: "toro-sponsor.firebasestorage.app",
+  messagingSenderId: "380071282395",
+  appId: "1:380071282395:web:71b1ec542fde866daf6efe"
+};
